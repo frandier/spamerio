964cfba0f09f35004ba591ae7dc799d0fa16f4c7
frandier <fabianfranco2001@gmail.com>
2022-10-09 18:08:40 -0400

    cleaning tweet

--- cleaner/cleaner.py
+++ cleaner/cleaner.py
@@ -6,4 +6,7 @@ class Cleaner:
         r = re.sub("@[A-Za-z0-9_]+","", r)
         r = re.sub("#[A-Za-z0-9_]+","", r)
         r = re.sub(r'http\S+', '', r)
+        r = re.sub('[()!?]', ' ', r)
+        r = re.sub('\[.*?\]',' ', r)
+        r = re.sub("[^a-z0-9]"," ", r)
         return r
